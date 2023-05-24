780f95c817ac9b019403591aef492225f74b5185
Jaspreet <jaspreetkn92@gmail.com>
2023-05-24 12:41:13 +0100

    added another test to the grouped testing

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -4,4 +4,8 @@ describe('fizzbuzz', () => {
   it('should be a function', () => {
     expect(typeof fizzBuzz).toEqual('function');
   });
+
+  it('should return the number if it is not divisible by 3 or 5', () => {
+    expect(fizzBuzz(1)).toEqual(1);
+  });
 });
